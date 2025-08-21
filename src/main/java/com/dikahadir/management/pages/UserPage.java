a4974b9477d2b213784f6109479df55a0ce89fcf
Denirudini <denirudini99@gmail.com>
2025-08-22 04:15:25 +0700

    update pendaftaran user

--- src/main/java/com/dikahadir/management/pages/UserPage.java
+++ src/main/java/com/dikahadir/management/pages/UserPage.java
@@ -3,8 +3,6 @@ package com.dikahadir.management.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-import com.dikahadir.management.utils.DriverUtil;
-
 public class UserPage {
     private WebDriver driver;
     private By buttonMenuManagement = By.xpath("//body/div[@id='__next']/div[@class='css-10f63sf']/div[@class='MuiBox-root css-k008qs']/div[@class='MuiDrawer-root MuiDrawer-docked css-1tu59u4']/div[@class='MuiPaper-root MuiPaper-elevation MuiPaper-elevation0 MuiDrawer-paper MuiDrawer-paperAnchorLeft MuiDrawer-paperAnchorDockedLeft css-1yw4omu']/div[@data-simplebar='init']/div[@class='simplebar-wrapper']/div[@class='simplebar-mask']/div[@class='simplebar-offset']/div[@aria-label='scrollable content']/div[@class='simplebar-content']/div[@class='MuiBox-root css-10klw3m']/div[@class='sidebar MuiBox-root css-ftf74w']/div[@class='MuiBox-root css-old1by']/div[4]/div[1]/div[1]//*[name()='svg']");
